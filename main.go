7297d2d9ca2f4b797459713873cea817cd2727e5
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-11 19:47:57 +1000

    structs

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	ys := map[string]string{"name": "ys", "age": "123"}
-	for key, value := range ys {
-		fmt.Println(key, value)
-	}
+	favFood1 := []string{"gummy", "chocolate"}
+	favFood2 := []string{"fruits", "beer"}
+
+	yeonseo := person{"yeonseo", 20, favFood1}
+	// yeonseo := person{"yeonseo", age:20, favFood1}
+	fmt.Println(yeonseo)
+	skye := person{name: "skye", age: 30, favFood: favFood2}
+	// skye := person{name: "skye", 30, favFood: favFood2}
+	fmt.Println(skye)
 }
