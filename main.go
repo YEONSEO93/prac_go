8d9440820a6cfee846a7a81b8b90660fb42524e1
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-11 19:12:48 +1000

    pointers

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	a := 2
+	b := &a
+	a = 10
+	fmt.Println(a, *b)
 }
