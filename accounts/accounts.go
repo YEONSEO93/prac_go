ff146ede9cc13598a0478ff8183b3fd6feb000ff
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-11 20:55:25 +1000

    BANK ACC

---
+++ accounts/accounts.go
@@ -0,0 +1,13 @@
+package accounts
+
+// Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 1000}
+	return &account
+}
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/YEONSEO93/learngo/accounts"
+)
 
 func main() {
-	favFood1 := []string{"gummy", "chocolate"}
-	favFood2 := []string{"fruits", "beer"}
-
-	yeonseo := person{"yeonseo", 20, favFood1}
-	// yeonseo := person{"yeonseo", age:20, favFood1}
-	fmt.Println(yeonseo)
-	skye := person{name: "skye", age: 30, favFood: favFood2}
-	// skye := person{name: "skye", 30, favFood: favFood2}
-	fmt.Println(skye)
+	account := accounts.NewAccount("yeonseo")
+	fmt.Println(account) // This will use the account variable
 }
