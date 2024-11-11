6ae877811140b812a3f033712a8cd346f5236298
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-11 21:41:09 +1000

    Update Acc

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	account := accounts.NewAccount("yeonseo")
 	fmt.Println(account)
 	account.Deposit(10)
-	fmt.Println(account.Balance())
+	// fmt.Println(account.Balance())
 	err := account.Withdraw(20)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
